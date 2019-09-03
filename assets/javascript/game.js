55575431be2ea36749fe480ce47798b3be1144ab
Christin Boner <christinmclemons@gmail.com>
2019-09-03 07:26:35 -0700

    got the random number to gues working!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,71 +1,56 @@
-//Crystal variables
-var blue = Math.floor(Math.random() * 12) + 1;
-var green = Math.floor(Math.random() * 12) + 1;
-var pink = Math.floor(Math.random() * 12) + 1;
-var red = Math.floor(Math.random() * 12) + 1;
-var buttons= ["#buttonBlue", "#buttonPink", "#buttonGreen", "#buttonRed"];
-//counters
+//a game with 4 crystals and a random goal#
+
+var goal_result;
+var lost = 0;
 var win = 0;
-var loss = 0;
-var finalScore
-var finalNumba;
-var goalNumba = Math.floor(Math.random() * 120) + 30;
-//Functions---------------
-function reset() 
-{
+var previous = 0;
 
-    goalNumba = Math.floor(Math.random() * 120) + 30;
-    document.getElementById("guess").innerHTML= goalNumba;
-    var blue = Math.floor(Math.random() * 12) + 1;
-    document.getElementById("buttonBlue").innerHTML = blue;
-    var green = Math.floor(Math.random() * 12) + 1;
-    document.getElementById("buttonGreen").innerHTML = green;
-    var pink = Math.floor(Math.random() * 12) + 1;
-    document.getElementById("buttonPink").innerHTML = pink;
-    var red = Math.floor(Math.random() * 12) + 1;
-    document.getElementById("buttonRed").innerHTML = red;
-    finalScore = 0;
-    start();
-}
 
-function start ()
-{
-    goalNumba = Math.floor(Math.random() * 120) + 30;
-    document.getElementById("guess").innerHTML = goalNumba;
-    var blue = Math.floor(Math.random() * 12) + 1;
-    document.getElementById("buttonBlue").innerHTML = blue;
-    var green = Math.floor(Math.random() * 12) + 1;
-    document.getElementById("buttonGreen").innerHTML = green;
-    var pink = Math.floor(Math.random() * 12) + 1;
-    document.getElementById("buttonPink").innerHTML = pink;
-    var red = Math.floor(Math.random() * 12) + 1;
-    document.getElementById("buttonRed").innerHTML = red;
-    finalScore = 0;
-
-}
-start()
-
-    
-
-//a game with 4 crystals and random result
 //every crystal needs to have a random number between 1-12
 //a new ranndom number should be generated every single to we win or lose
 //to those 4 crytals
-//when clicking any crystal, it should be adding the value to the previous result
+function resetAndStart() {
+    var goal_result = Math.floor(Math.random() * 120) + 30;
+    document.getElementById('number-to-guess').innerHTML = goal_result;
+};
 
-//until it is equal or greater than the random result
-//if it is equal, increment the wins
-if(goalNumba === finalNumba)
-{
-    win++;
-    $('.win').html(win);
-    reset();
+    //for(var i = 0; i<1 ; i++) {}
+
+    //var random = Math.floor(Math.random() * 12) + 1;
+
+//<button onclick="document.getElementById('demo').innerHTML = Date()">The time is?</button>
     
-}
-//if it is greater than the random result, increment the losses
-else if (finalNumba < goalNumba){
-    losses++;
-    $('.losses').html(losses);   
-    reset();
+    resetAndStart()
 
-}
+
+$(document).on('click', ".crystal", function () {
+    
+    var num = parseInt($(this).attr('data-random'));
+    
+    //when clicking any crystal, it should be adding the value to the previous result
+    previous += num;
+    
+    $("#previous").html(+ previous);
+    
+    console.log(previous);
+
+    //until it is equal or greater than the random result
+    
+    //if it is greater than the random result, increment the losses
+    if (previous > goal_result) {
+        lost--;
+        $("#lost").html(+ lost);
+        previous = 0;
+        alert("Better luck next time!");
+        resetAndStart();
+    }
+    //if it is equal, increment the wins
+    else if (previous === goal_result) {
+        win++;
+        $("#win").html(+ win);
+        previous = 0;
+        alert("You did it!");
+        resetAndStart();
+    }
+});
+
