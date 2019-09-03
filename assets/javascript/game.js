e085aff1e5aa8936f9178eca4b5d1f290aceefae
Christin Boner <christinmclemons@gmail.com>
2019-09-02 18:18:29 -0700

    associating random numbers with gem

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,106 +1,71 @@
-//when the document is ready load this page
+//Crystal variables
+var blue = Math.floor(Math.random() * 12) + 1;
+var green = Math.floor(Math.random() * 12) + 1;
+var pink = Math.floor(Math.random() * 12) + 1;
+var red = Math.floor(Math.random() * 12) + 1;
+var buttons= ["#buttonBlue", "#buttonPink", "#buttonGreen", "#buttonRed"];
+//counters
+var win = 0;
+var loss = 0;
+var finalScore
+var finalNumba;
+var goalNumba = Math.floor(Math.random() * 120) + 30;
+//Functions---------------
+function reset() 
+{
 
-// targetNumber keeps track of target number to be reached
-
-// guessNumber keeps track of number as we guess
-//guessNumber is the sum of each gem clicked
-
-// gems blue green pink red keeps track of each button
-
-// wins keeps track of wins = 0
-
-// losses keeps track of loses = 0
-
-//---------------------------------------------------------
-
-//targetNumber number is randomly generated
-//gems generates a random number for each gem 
-
-//if guessNumber is equal to the TargetNumber
-//add one to wins
-//start over again
-
-//if guessNumber is more than the targetNumber
-//add one to the losses
-//start over again
-//----------------------------------------------
-
-//each gem is listening for a click
-//if it hears a click add the value of itself to guessNumber
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//THIS IS THE CRYSTAL ACTIVITY CHEAT SHEET
-
-
-var targetNumber = 53;
-
-$("#number-to-guess").text(targetNumber);
-
-var counter = 0;
-
-// Now for the hard part. Creating multiple crystals each with their own unique number value.
-
-// We begin by expanding our array to include four options.
-var numberOptions = [10, 5, 3, 7];
-
-// Next we create a for loop to create crystals for every numberOption.
-for (var i = 0; i < numberOptions.length; i++) {
-
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-
-    // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
+    goalNumba = Math.floor(Math.random() * 120) + 30;
+    document.getElementById("guess").innerHTML= goalNumba;
+    var blue = Math.floor(Math.random() * 12) + 1;
+    document.getElementById("buttonBlue").innerHTML = blue;
+    var green = Math.floor(Math.random() * 12) + 1;
+    document.getElementById("buttonGreen").innerHTML = green;
+    var pink = Math.floor(Math.random() * 12) + 1;
+    document.getElementById("buttonPink").innerHTML = pink;
+    var red = Math.floor(Math.random() * 12) + 1;
+    document.getElementById("buttonRed").innerHTML = red;
+    finalScore = 0;
+    start();
 }
 
-// This time, our click event applies to every single crystal on the page. Not just one.
-$(".crystal-image").on("click", function () {
+function start ()
+{
+    goalNumba = Math.floor(Math.random() * 120) + 30;
+    document.getElementById("guess").innerHTML = goalNumba;
+    var blue = Math.floor(Math.random() * 12) + 1;
+    document.getElementById("buttonBlue").innerHTML = blue;
+    var green = Math.floor(Math.random() * 12) + 1;
+    document.getElementById("buttonGreen").innerHTML = green;
+    var pink = Math.floor(Math.random() * 12) + 1;
+    document.getElementById("buttonPink").innerHTML = pink;
+    var red = Math.floor(Math.random() * 12) + 1;
+    document.getElementById("buttonRed").innerHTML = red;
+    finalScore = 0;
 
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+}
+start()
 
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
+    
 
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
-    alert("New score: " + counter);
+//a game with 4 crystals and random result
+//every crystal needs to have a random number between 1-12
+//a new ranndom number should be generated every single to we win or lose
+//to those 4 crytals
+//when clicking any crystal, it should be adding the value to the previous result
 
-    if (counter === targetNumber) {
-        alert("You win!");
-    }
+//until it is equal or greater than the random result
+//if it is equal, increment the wins
+if(goalNumba === finalNumba)
+{
+    win++;
+    $('.win').html(win);
+    reset();
+    
+}
+//if it is greater than the random result, increment the losses
+else if (finalNumba < goalNumba){
+    losses++;
+    $('.losses').html(losses);   
+    reset();
 
-    else if (counter >= targetNumber) {
-        alert("You lose!!");
-    }
-
-});
+}
