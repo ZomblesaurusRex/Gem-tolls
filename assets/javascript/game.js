98a4c7f9f8b55b009710b521bb960e73249f01ac
Christin Boner <christinmclemons@gmail.com>
2019-09-03 22:56:03 -0700

    what I could still NaN

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,75 +8,85 @@ var blue;
 var green;
 var red;
 var pink;
-
+//
 //a new random number is generated as the ultimate number to achieve
 function goalNumber() {
     var goal_result = Math.floor(Math.random() * 120) + 30;
     document.getElementById('number-to-guess').innerHTML = goal_result;
 };
 
+var numberOptions = [5, 2, 8, 4, 1, 10, 7, 12, 9, 6, 11, 3];
+var crystalSrc = ["assets/images/bluejewel.png", "assets/images/greenjewel.png", "assets/images/pinkjewel.png", "assets/images/redjewel.png"]
+//var rando = {numberOptions: [Math.floor(Math.random() * numberOptions.length)]}
 //every crystal needs to have a random number between 1-12
-function blueRandoGem() {
-    var blueRando = Math.floor(Math.random() * 12) + 1;
-    document.getElementById('blueGem').innerHTML = blueRando;
-};
-function pinkRandoGem() {
-    var pinkRando = Math.floor(Math.random() * 12) + 1;
-    document.getElementById('pinkGem').innerHTML = pinkRando;
-};
-function greenRandoGem() {
-    var greenRando = Math.floor(Math.random() * 12) + 1;
-    document.getElementById('greenGem').innerHTML = greenRando;
-};
-function redRandoGem() {
-    var redRando = Math.floor(Math.random() * 12) + 1;
-    document.getElementById('redGem').innerHTML = redRando;
-};
-//
+// Next we create a for loop to create crystals for every numberOption.
+for (var i = 0; i < crystalSrc.length; i++) {
 
-//onclick="document.getElementById('previous').innerHTML = 
-//a new ranndom number should be generated every single to we win or lose
+    // For each iteration, we will create an imageCrystal
+    var imageCrystal = $("<img>");
+
+    // First each crystal will be given the class ".crystal-image".
+    // This will allow the CSS to take effect.
+    imageCrystal.addClass("button");
+
+    // Each imageCrystal will be given a src link to the crystal image
+    imageCrystal.attr("src", crystalSrc[i]);
+
+    // Each imageCrystal will be given a data attribute called data-crystalValue.
+    // This data attribute will be set equal to the array value.
+    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+
+    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+    $("#crystals").append(imageCrystal);
+}
+//a new random number should be generated every single to we win or lose
 //to those 4 crytals
 //for(var i = 0; i<1 ; i++) {}
-blueRandoGem()
-pinkRandoGem()
-redRandoGem()
-greenRandoGem()
-    //var random = Math.floor(Math.random() * 12) + 1;
-
 
 //generates the number in number to guess section 
 goalNumber()
 
 
-$(document).on('click', function () {
-    
-    var num = parseInt($(this).attr('data-random'));
-    
-    //when clicking any crystal, it should be adding the value to the previous result
-    previous += num;
-    
-    $("#previous").html(+ previous);
-    
-    console.log(previous);
+//var random = Math.floor(Math.random() * 12) + 1; 
+//when clicking any crystal, it should be adding the value to the previous result
+$("#crystals").on("click", function () {
+    //
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    // We then add the crystalValue to the user's "counter" which is a global variable.
+    // Every click, from every crystal adds to the global counter.
+    previous += crystalValue
+
+    $("#previousNum").html(+ previous);
+
+    alert("New score: " + previous);
+
+    if (previous === goal_result) {
+        alert("You win!");
+    }
+
+    else if (previous >= goal_number) {
+        alert("You lose!!");
+    }
+    //document.getElementById("previousNum").innerHTML = previous;
+
 
     //until it is equal or greater than the random result
-    
     //if it is greater than the random result, increment the losses
-    if (previous > goal_result) {
-        lost--;
-        $("#lost").html(+ lost);
-        previous = 0;
-        alert("Better luck next time!");
-        resetAndStart();
-    }
-    //if it is equal, increment the wins
-    else if (previous === goal_result) {
-        win++;
-        $("#win").html(+ win);
-        previous = 0;
-        alert("You did it!");
-        resetAndStart();
-    }
-});
+    //if (previous > goal_result) {
+    //  lost++;
+    //$("#lost").html(+ lost);
+    // alert("Better luck next time!");
+    ///   previous = 0;
+    /// goalNumber();
 
+    //if it is equal, increment the wins
+    //else if (previous === goal_result) {
+    //  win++;
+    //$("#win").text(+ win);
+    // alert("You did it!");
+    // previous = 0;
+    //goalNumber();
+}
+);
